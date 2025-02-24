5cef22569e64a1481a0e4e82750e9f87eb67757e
Jkssns <zhangjun035@126.com>
2025-02-24 02:39:20 +0000

    update

--- compareAndUpdate.js
+++ compareAndUpdate.js
@@ -9,7 +9,7 @@ const formatDate = (date) => {
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return ;
+  return [year, '-', month, '-', day, ' ', hours, ':', minutes, ':', seconds].join('')
 }
 
 try {
