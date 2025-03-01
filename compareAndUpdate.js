3ba4d27e0b99e26726ff714180eb149786549f5d
Jkssns <Jkssns@users.noreply.github.com>
2025-03-01 09:14:01 +0000

    deploy: 7c4faa4e35dfb4cfbad02e60daadb8a60844d691

--- compareAndUpdate.js
+++ compareAndUpdate.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
-console.log('11: ', {"ip_location":"辽宁","following_count":158,"follower_count":52,"favoriting_count":1610});
-const item = JSON.parse('{"ip_location":"辽宁","following_count":158,"follower_count":52,"favoriting_count":1610}');
-const data = require('./dataxin.json')
+const item = JSON.parse('{"ip_location":"北京","following_count":6,"follower_count":27,"favoriting_count":143}');
+const data = require('./datame.json')
 const formatDate = (date) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -24,19 +23,14 @@ const isSame =
   item.following_count === lastItem.following_count &&
   item.follower_count === lastItem.follower_count &&
   item.favoriting_count === lastItem.favoriting_count;
-  console.log('item::: ', item)
-  console.log('formatDate::: ', formatDate);
-  console.log('formatDate::: ', formatDate(new Date()));
 if (!isSame) {
   // 若不一致，将 item 添加到 data 中
   item.update_time = formatDate(new Date());
-  console.log('item::: ', item);
   data.push(item);
   // 将更新后的数据转换为字符串，并添加 var data = 和 ]
   const newData = JSON.stringify(data, null, 2)
   // 将新数据写入 data.json 文件
   fs.writeFileSync('data.json', newData);
-  console.log('data::: ', data);
   console.log('Data has been updated in data.json.');
 } else {
   console.log('The data in  and the last item in data.json are the same. No update needed.');
